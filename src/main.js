3645470b894e0cb3c7ed96398c76e4d1d7e900a6
claudiordgz <claudio.rodriguez@coxinc.com>
2016-01-25 20:28:33 -0500

    feat: more slides

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 "use strict";
 require('reveal.js/css/reveal.css');
 require('reveal.js/css/theme/solarized.css');
+require('reveal.js/lib/css/zenburn.css');
 
 require('script!reveal.js/js/reveal.js');
-
+require('script!reveal.js/plugin/highlight/highlight.js');
 require('script!tether/dist/js/tether.min.js');
 require('bootstrap-loader');
 
 require('./presentation/entry-point.js');
 
 
-Reveal.initialize();
+Reveal.initialize({ backgroundTransition: 'zoom' });
+hljs.initHighlightingOnLoad();
